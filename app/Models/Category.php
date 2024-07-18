65e53d70b7f0d17c8773baa6d6bbf469d9e4768c
roomdada <dsieroger@gmail.com>
2024-07-18 22:51:53 +0100

    fix

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description'];
+
+    public function events()
+    {
+        return $this->hasMany(Event::class, 'category_id');
+    }
 }
